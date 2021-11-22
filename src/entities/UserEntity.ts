d878c85f746accca4a26883fb85b71a9f923b006
gabrielbugarelli <gabrielfonsecaoficial@gmail.com>
2021-11-22 14:37:44 -0400

    feat: implements user entity

---
+++ src/entities/UserEntity.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity("users")
+export class UserEntity {
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  email: string;
+  password: string;
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
