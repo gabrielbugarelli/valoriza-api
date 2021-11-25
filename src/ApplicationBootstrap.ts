4e762a5efeac71122f453b6555b3e77d4b19e1e8
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-11-25 13:25:42 -0400

    feat: tratando exceções

--- src/ApplicationBootstrap.ts
+++ src/ApplicationBootstrap.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "reflect-metadata";
 import { router } from "./routes";
+import "express-async-errors";
 import "./database";
 
 const applicationBootstrap = express();
@@ -12,6 +13,20 @@ applicationBootstrap.use(router);
 
 applicationBootstrap.use(express.json());
 
+
+applicationBootstrap.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if(error instanceof Error) {
+    return response.status(400).json({
+      error: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
+
 applicationBootstrap.listen(PORT, () => {
   console.log(`Application started on port: ${PORT}`);
-});
+}); 
