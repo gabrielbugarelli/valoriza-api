c2b0e2efb89243c8af8a9f251037049f160451cc
Gabriel <gabrielfonsecaoficial@gmail.com>
2021-11-28 16:19:59 -0400

    feat: criaçao do CreateTagController

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 export const router = Router();
 const createUserController = new CreateUserController();
+const createTagControlller = new CreateTagController();
 
 router.post('/users', createUserController.handle);
+router.post('/tags', createTagControlller.handle);
