9ec6cbc61a21a1b51fe3cd8031912d5fa941f951
Gabriel <gabrielfonsecaoficial@gmail.com>
2021-11-28 19:14:47 -0400

    feat: regra para impedir cadastro de tags para usuários não autenticados

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 export const router = Router();
 const createUserController = new CreateUserController();
 const createTagControlller = new CreateTagController();
 
+router.post('/tags', ensureAdmin, createTagControlller.handle);
 router.post('/users', createUserController.handle);
-router.post('/tags', createTagControlller.handle);
